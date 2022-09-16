d8c3049fc468136a7b764da1a8dea1f4a8338d3f
Naveennayak1 <107264965+Naveennayak1@users.noreply.github.com>
2022-09-16 08:08:32 +0530

    tinder app in react

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import List from "./components/list/index"
+
+const Home = () => {
+  return (
+    <div>
+   <Header />
+   <List />
+<Footer />
+
+    </div>
+  );
+};
+
+export default Home;
