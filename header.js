3a3e138511ebb7726c3963afaf1311b16e3f67b8
Naveennayak1 <107264965+Naveennayak1@users.noreply.github.com>
2022-09-16 08:12:57 +0530

    tinder app

---
+++ header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./header.module.css";
+
+const Header =() =>{
+return(
+  <header className={classes.container}>
+<div className={classes.logo}>
+            <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53b.png" />
+
+</div>
+
+
+  </header>
+)
+
+
+}
+export default Header
