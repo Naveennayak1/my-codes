703048c09e780656c4523d2b0d7f61890b6a9609
Naveennayak1 <107264965+Naveennayak1@users.noreply.github.com>
2022-10-10 19:07:31 +0530

    my kafene project
    this is my exam project

--- index.js
+++ index.js
@@ -1,24 +1,77 @@
-import React,{usestate} from "react";
- import styles from "./searchbar.module.css";
+$(document).ready(() => {
 
- const Searchbar =(props) => {
-   const{ search, onSearchChange} =props;
-   const onChangeSearch =(e) => {
+    $('.menu-items').click((e) => {
 
-const value =e.target.value;
-onSearchChange(value);
+        e.preventDefault()
 
-   };
-   
-   return(
-    <div className="styles.container">
-        <input className={styles.input} placehoolder="searchusername" value={search} onChange={onChangeSearch} />
-    </div>
-
-   
-   );
-   
+        if(!loginStatus) {
+            location.assign('/index.html')
+        }
+    })
 
 
- };
- export default Searchbar;
+
+    $('#login-btn').click((e) => {
+        e.preventDefault()
+
+        let username = $('#login-name').val()
+        let password = $('#login-password').val()
+
+        let userDetails = JSON.parse(localStorage.getItem('user-details'))
+
+        if(userDetails == null && userDetails == undefined) {
+            alert('Invalid Credentials')
+        }
+        else if(userDetails.name == username && userDetails.password == password) {
+            alert('Login Successful!')
+            let logoutBtn = $('.menu-items')
+
+            console.log(logoutBtn)
+            logoutBtn.addClass('loggedIn').removeClass('loggedOut')
+
+            location.assign('./Pages/Orders/orders.html')
+        }
+
+    })
+
+    $('#sign-up').click((e) => {
+        let loginForm = $('#login-form')
+        loginForm.addClass('inactive-form')
+        loginForm.removeClass('active-form')
+
+        let signUpForm = $('#sign-up-form')
+        signUpForm.addClass('active-form')
+        signUpForm.removeClass('inactive-form')
+
+        let formHeading = $('#form-heading')
+        formHeading.text('Sign Up')
+    })
+
+    $('#sign-up-btn').click((e) => {
+        e.preventDefault()
+
+        mObj = {
+            name: $('#sign-up-name').val(),
+            password: $('#sign-up-password').val()
+        }
+
+        if(mObj.name != mObj.password) {
+            alert('PLease enter correct Credentials to Sign up!')
+        } else {
+            localStorage.setItem('user-details', JSON.stringify(mObj))
+
+            let loginForm = $('#login-form')
+            loginForm.addClass('active-form')
+            loginForm.removeClass('inactive-form')
+
+            let signUpForm = $('#sign-up-form')
+            signUpForm.addClass('inactive-form')
+            signUpForm.removeClass('active-form')
+
+            let formHeading = $('#form-heading')
+            formHeading.text('Sign Ip')
+
+            location.assign('./index.html')
+        }
+    })
+})
